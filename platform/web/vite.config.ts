591b53c1946ded8546e3f0680baddca478d9cd07
CheerioInf <109421039+cheerioInf@users.noreply.github.com>
2023-11-13 16:18:38 +0800

    feat: update repository page

--- platform/web/vite.config.ts
+++ platform/web/vite.config.ts
@@ -4,5 +4,14 @@ import viteEslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteEslint()]
+	plugins: [react(), viteEslint()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://43.136.166.192:8089",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, "/api")
+			}
+		}
+	}
 });
